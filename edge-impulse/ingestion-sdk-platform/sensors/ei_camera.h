ef93be464133c1537d619334dfea5885ace8fd7d
Dhruba Saha <dhrubasaha08@gmail.com>
2024-02-20 23:33:14 +0530

    Update ei_camera.h
    Update header

--- edge-impulse/ingestion-sdk-platform/sensors/ei_camera.h
+++ edge-impulse/ingestion-sdk-platform/sensors/ei_camera.h
@@ -1,5 +1,6 @@
 /* Edge Impulse ingestion SDK
  * Copyright (c) 2022 EdgeImpulse Inc.
+ * Modified by @dhrubasaha08
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
